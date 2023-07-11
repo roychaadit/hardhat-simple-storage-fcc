08a884eb34a29f137a2877b5674e87d1de33063b
roychaadit <roychowdhuryaadit@gmail.com>
2023-07-11 17:29:47 +0530

    Tests, tasks, libraries

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
-require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
-    defaultNetwork: "hardhat", // No need to be explicit as hardhat implicitly runs itslef in the background
+    defaultNetwork: "hardhat",
     networks: {
+        hardhat: {},
         sepolia: {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
@@ -20,5 +24,17 @@ module.exports = {
     solidity: "0.8.18",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    localhost: {
+        url: "http://127.0.0.1:8545/",
+        chainId: 31337,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
     },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,7 @@
+// imports
 const { ethers, run, network } = require("hardhat")
 
+// async main
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
         "SimpleStorage",
@@ -8,10 +10,25 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deploymentTransaction().wait()
     console.log(`Deployed contract to: ${simpleStorage.target}`)
-    console.log(network.config)
+    // console.log(network.config)
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block confirmations...")
+        await simpleStorage.deploymentTransaction().wait(6)
+        await verify(simpleStorage.target, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(21)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is: ${updatedValue}`)
 }
 
-async function verify(contractAddress, args) {
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
     console.log("Verifying contract...")
     try {
         await run("verify:verify", {
@@ -20,13 +37,14 @@ async function verify(contractAddress, args) {
         })
     } catch (e) {
         if (e.message.toLowerCase().includes("already verified")) {
-            console.log("Already verified")
+            console.log("Already Verified!")
         } else {
             console.log(e)
         }
     }
 }
 
+// main
 main()
     .then(() => process.exit(0))
     .catch((error) => {
